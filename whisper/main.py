2a093c8df3ebd7722f8079e55d71172ffa84951a
jotpalch <jotp076315217@gmail.com>
2024-05-13 19:49:15 +0800

    feat: add youtube url support

--- whisper/main.py
+++ whisper/main.py
@@ -3,13 +3,38 @@ import os
 from fastapi import FastAPI, UploadFile, status, Response, BackgroundTasks
 
 from handler import audio, file
-from util import hash
+from model.audio_model import AudioModel
+from pytube import YouTube
+import moviepy.editor
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "FastAPI"}
+
+@app.get("/youtube")
+async def get_youtube_mp3(background_tasks: BackgroundTasks, url: str):
+
+    id = generate(size=15)
+    video_path = f"tmp/{id}/temp_video.mp4"
+    audio_path = f"tmp/{id}/temp_audio.mp3"
+
+    # Download YouTube video
+    youtube = YouTube(url)
+    video = youtube.streams.get_lowest_resolution()
+    video.download(output_path=f"tmp/{id}", filename="temp_video.mp4")
+
+    # Convert video to mp3
+    video = moviepy.editor.VideoFileClip(video_path)
+    clip_audio = video.audio
+    clip_audio.write_audiofile(audio_path)
+
+    background_tasks.add_task(audio.generate_transcript, audio_path)
+
+    return {
+        "id": id
+    }
 
 @app.post("/audio", status_code=201)
 async def post_audio(up_file: UploadFile, background_tasks: BackgroundTasks):
